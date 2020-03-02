b1d945ff4ed1b33eec59a39f2eb399efbbafb898
chen <wwwcjf163@163.com>
2020-03-02 11:28:42 +0800

    提交证书验证文件

--- server/index.js
+++ server/index.js
@@ -1,38 +1,45 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const routes = require('./routes');
-const errorHandler = require('./middlewares/').errorHandler;
+const _ = require("lodash");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const routes = require("./routes");
+const errorHandler = require("./middlewares/").errorHandler;
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  const webpack = require('webpack');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const webpackConfig = require('../webpack.config.dev');
+if (process.env.NODE_ENV === "development") {
+  const webpack = require("webpack");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
+  const webpackConfig = require("../webpack.config.dev");
 
   const compiler = webpack(webpackConfig);
 
-  app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    noInfo: true
-  }));
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+      noInfo: true
+    })
+  );
   app.use(webpackHotMiddleware(compiler));
 }
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.use(
+  "/.well-known",
+  express.static(path.join(__dirname, "static/.well-known"))
+);
+
 app.use(routes);
 app.use(errorHandler);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 module.exports = app;
